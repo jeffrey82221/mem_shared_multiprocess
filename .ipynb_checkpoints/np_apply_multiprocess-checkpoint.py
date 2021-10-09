228a553043753ab0fbf18468dc7b52d1b48653bc
jeffrey82221 <jeffrey82221@gmail.com>
2021-10-09 16:52:23 +0800

    commit

--- .ipynb_checkpoints/np_apply_multiprocess-checkpoint.py
+++ .ipynb_checkpoints/np_apply_multiprocess-checkpoint.py
@@ -22,8 +22,26 @@ import sys
 import psutil
 import mmap
 import time 
-
 CPU_CNT = os.cpu_count()
+def slice_generator(array, cpu_cnt = 1):
+    assert cpu_cnt >= 1
+    size = int(np.ceil(len(fp)/cpu_cnt))
+    i = 0 
+    while True:
+        try:
+            ans = array[i: i + size]
+            if len(ans) == size:
+                yield ans 
+            else:
+                raise ValueError('size not enough')
+            i += size
+        except:
+            break
+    ans = array[i:]
+    if len(ans) > 0:
+        yield ans 
+    else:
+        pass
 
 def multiprocess_map(func, array, **kwargs):
     """
@@ -33,6 +51,9 @@ def multiprocess_map(func, array, **kwargs):
     Args: 
         - func : function to be apply to array[i] 
         - array : a numpy array. 
+        
+    Notes: 
+        - pid_records: records whether a child is finished. 
     
     TODO: 
     - [X] make sure all child is stopped before existing the parent function 
@@ -45,9 +66,9 @@ def multiprocess_map(func, array, **kwargs):
     - [ ] read as 'c' mode (write read availble on ram but read only on disk) 
     - [ ] remove the file once calculation done. 
     """
-    process_cnt = len(mmap_array)
-    # pid_records records whether a child is finished. 
+    process_cnt = len(array)
     pid_records = mmap.mmap(-1, length=process_cnt, access=mmap.ACCESS_WRITE)
+    
     for i in range(process_cnt):
         pid = os.fork()
         if pid == 0:
@@ -58,7 +79,7 @@ def multiprocess_map(func, array, **kwargs):
             pass 
     while pid_records[:] != b'\x01' * process_cnt:
         continue
-    return mmap_array
+    return array
 
 from tempfile import mkdtemp
 import mmap
@@ -67,18 +88,23 @@ import os
 import os.path as path
 import numpy as np 
 
-def assign_position(element):
-    element += 2
+def assign_position(element, plus_number = 1):
+    element += plus_number
     element[0] = element[1] + element[2]
-    element[:] = element[:] + element ** 2
+    return element
     
 if __name__ == '__main__':
 
+    input_a = np.zeros((10,4))
+    
+    ans_single_cpu = np.apply_along_axis(assign_position, 1, input_a, plus_number = 1)
+    print(ans_single_cpu)
+    
     input_a = np.zeros((10,4))
     filename = path.join(mkdtemp(), 'tmp_numpy.dat')
     fp = np.memmap(filename, dtype=input_a.dtype, mode='w+', shape=input_a.shape)
     fp[:] = input_a[:]
     print('Start MultiProcess')
-    ans = multiprocess_map(assign_position, fp)
+    ans = multiprocess_map(assign_position, fp, plus_number = 1)
     print('End MultiProcess')
     print(ans)
