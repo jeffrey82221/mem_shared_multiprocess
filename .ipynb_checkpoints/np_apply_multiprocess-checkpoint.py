8ae2df6dbf6a4062e0fdef9904864d61e1f3dd0b
jeffrey82221 <jeffrey82221@gmail.com>
2021-10-09 18:39:51 +0800

    replace np.memmap with sharedmem, which create shared memory without a temp file in disk

--- .ipynb_checkpoints/np_apply_multiprocess-checkpoint.py
+++ .ipynb_checkpoints/np_apply_multiprocess-checkpoint.py
@@ -26,6 +26,8 @@ import time
 from tempfile import mkdtemp
 import os.path as path
 import numpy as np 
+import sharedmem
+# https://github.com/rainwoodman/sharedmem
 
 def slice_generator(array, cpu_cnt = 1):
     assert cpu_cnt >= 1
@@ -48,14 +50,16 @@ def slice_generator(array, cpu_cnt = 1):
         pass
     
 def create_share_mem_array(array):
-    filename = path.join(mkdtemp(), 'tmp_numpy.dat')
-    fp = np.memmap(filename, dtype=array.dtype, mode='w+', shape=array.shape)
+    #filename = path.join(mkdtemp(), 'tmp_numpy.dat')
+    #fp = np.memmap(filename, dtype=array.dtype, mode='w+', shape=array.shape)
+    fp = sharedmem.empty(array.shape, dtype=array.dtype)
+
     fp[:] = array[:]
     #fp.flush()
     #del fp 
     #gc.collect()
     #fp = np.memmap(filename, dtype=array.dtype, mode='c', shape=array.shape)
-    return fp, filename
+    return fp
 
 def multiprocess_map(func, input_array, **kwargs):
     """
@@ -79,7 +83,7 @@ def multiprocess_map(func, input_array, **kwargs):
     - [X] save empty numpy to file 
     - [X] remove the file once calculation done. 
     """
-    array, filename = create_share_mem_array(input_array)
+    array = create_share_mem_array(input_array)
     process_cnt = os.cpu_count()
     pid_records = mmap.mmap(-1, length=process_cnt, access=mmap.ACCESS_WRITE)
     pids = []
@@ -99,12 +103,20 @@ def multiprocess_map(func, input_array, **kwargs):
     while pid_records[:] != b'\x01' * process_cnt:
         continue
     result = np.array(array)
-    os.remove(filename)
     del array
     gc.collect()
     return result
 
-    
+################################################################################
+
+def assign_position(in_element, plus_number = 1):
+    element = in_element.copy()
+    element += plus_number
+    for i in range(1000):
+        element[0] = element[0] + element[1] + element[2]
+        element[1] = element[0] + element[2]
+    return element
+
 if __name__ == '__main__':
 
     input_a = np.ones((10,4))
@@ -116,9 +128,12 @@ if __name__ == '__main__':
     
     
     print('MultiProcess:\n')
-    ans = multiprocess_map(assign_position, input_a, plus_number = 1)
-    print(ans)
+    ans_multi_cpu = multiprocess_map(assign_position, input_a, plus_number = 1)
+    print(ans_multi_cpu)
+    
+    identical = np.all(ans_multi_cpu == ans_single_cpu)
+    assert identical
+    print('Identical:', identical)
     
     print('Original:\n')
     print(input_a)
-    
